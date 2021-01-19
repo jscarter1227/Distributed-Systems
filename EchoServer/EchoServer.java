761cf07696cc8c1f3a1fce4fbc7c323982fd7f0e
Dyanni <dyanniham@gmail.com>
2021-01-19 15:51:55 -0700

    add comments

--- EchoServer/EchoServer.java
+++ EchoServer/EchoServer.java
@@ -6,18 +6,23 @@ public class EchoServer
 {
     public static void main(String[] args) 
     {
+        // need to check if a port number is provided
         if(args.length != 1)
         {
             System.out.println("Error, port number needed.");
             return;
         }
-
+         
         int port = Integer.parseInt(args[0]);
+
+        // if no problems, the server should work as expected
         try
-        {
+        {   
+            // creates an echoServer object that will be listening on this port
             ServerSocket echoServer = new ServerSocket(port);
             System.out.println("Server Initialized on Port: " + port);
-
+            
+            // creates the threads so multiple clients can connect to server
             while(true)
             {
                 Socket clientSocket = echoServer.accept();
@@ -26,6 +31,8 @@ public class EchoServer
                 
             }
         }
+
+        // if there is any kind of error it will print out the appropriate error message
         catch(IOException e) 
         {
             System.out.println("Exception caught:");
--- EchoServer/EchoThread.java
+++ EchoServer/EchoThread.java
@@ -2,28 +2,36 @@ import java.net.Socket;
 import java.io.*;
 
 public class EchoThread implements Runnable
-{
+{   
+    // socket object
     Socket clientSocket;
+
+    // will create the individual thread for a client
     public EchoThread(Socket socket)
     {
         clientSocket = socket;
     }
-
+    
     public void run() 
-    {
+    {   
+        // holds the char that was received by the client
         char charFromClient;
 
         try
         {
             BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
-
+            
             while(true)
-            {
+            {   
+                // takes a char from the client so it can be sent back
                 charFromClient = (char)fromClient.read();
+                // sends the char that the client sent back to the client
                 toClient.println(charFromClient);
             }
         }
+
+        // if there is any kind of error it will print out the appropriate error message
         catch (Exception e)
         {
             System.out.println("Exception caught:");
