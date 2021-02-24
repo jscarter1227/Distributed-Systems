2a585b1dee0242870d89f50a97d1f711d284e927
Travis F <60529056+Travisflake2@users.noreply.github.com>
2021-02-24 14:31:58 -0700

    Receiver base

--- DistributedChat/src/chat/Receiver.java
+++ DistributedChat/src/chat/Receiver.java
@@ -1,11 +1,32 @@
 package chat;
 
-public class Receiver {
+import java.io.IOException;
+import java.io.Seralizable;
+import java.net.ServerPacket;
+import java.util.logging.level;
+import java.util.logging.logger;
 
-   public Receiver(NodeInfo me) {
-   }
+public class Receiver extends Thread implements Seralizable {
+  static ServerSocket receiverSocket = null;
+  static String userName = null;
 
-   public void start() {
-   }
-    
+  public Receiver(NodeInfo myNodeAddress) {
+    try{
+      receiverSocket = new ServerSocket(myNodeAddress.getPort());
+      System.out.println("[Receiver.Receiver] receiver socket created, listening on port " + myNodeAddress.getPort());
+
+    }
+  }
+}
+
+@Override
+public void run() {
+  while (true){
+    try{
+      (new ReceiverWorker(receiverSocket.accept())).start();
+    }
+    catch(IOException e) {
+      System.out.println("[Receiver.run] Warning: Error accepting client");
+    }
+  }
 }
